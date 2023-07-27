59d990da5dc4b30dcdc8511aecdbb119287ed550
jonn <joliversalamanca@gmail.com>
2023-07-27 16:35:22 -0400

    new: anotaciones.md , api.json, index.html, javascript.js, style.css

---
+++ javascript.js
@@ -0,0 +1,97 @@
+const $carts = document.getElementById('carts')
+const $items = document.getElementById('items')
+const $footer = document.getElementById('footer')
+
+const $templateCarrito = document.getElementById('template-carrito').content
+const $templateFooter = document.getElementById('template-carrito').content
+const $template = document.getElementById('template-card').content
+const $fragment = document.createDocumentFragment()
+let carrito = {}
+
+const fetchData = async ()=>{
+    try {
+        const respuesta = await fetch('api.json')
+        const json = await respuesta.json()
+        render(json)
+    } catch (error) {
+        console.log(error)    
+    }
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetchData()
+})
+
+const render = (productos) =>{
+    productos.forEach((producto) => {
+        $template.querySelector('h5').textContent = producto.title
+        $template.querySelector('p').textContent = producto.precio
+        $template.querySelector('img').setAttribute('src', producto.thumbnailUrl)
+        $template.querySelector('img').setAttribute('alt', producto.title)
+        $template.querySelector('button').dataset.id = producto.id
+        const clone = $template.cloneNode(true)
+        $fragment.appendChild(clone)
+    });
+    $carts.appendChild($fragment)
+}
+
+$carts.addEventListener('click',e =>{
+    addCarrito(e)
+})
+
+const addCarrito = (e)=>{
+
+    if(e.target.classList.contains('btn-dark')){
+        setCarrito(e.target.parentElement)
+    }    
+}
+
+const setCarrito = (objeto)=>{
+    const producto = {
+        id:objeto.querySelector('.btn-dark').dataset.id,
+        title:objeto.querySelector('h5').textContent,
+        precio:objeto.querySelector('p').textContent,
+        cantidad:1
+    }
+
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad +1
+    }
+
+    carrito[producto.id] = {...producto}
+    registroCarrito()
+}
+
+const registroCarrito = ()=>{
+    $items.innerHTML = ''
+    Object.values(carrito).forEach(producto =>{
+        $templateCarrito.querySelector('th').textContent = producto.id
+        $templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        $templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        $templateCarrito.querySelector('.btn-info').textContent = producto.id
+        $templateCarrito.querySelector('.btn-danger').textContent = producto.id
+        $templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+
+        $clone = $templateCarrito.cloneNode(true)
+        $fragment.appendChild($clone)
+    })     
+
+    $items.appendChild($fragment)
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
